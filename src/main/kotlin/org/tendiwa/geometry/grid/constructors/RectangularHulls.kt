d02230eca6679f39abbb52d82b29212ab66bc554
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-10 09:15:04 +0500

    Add rectangular hull function for a set of tiles

--- src/main/kotlin/org/tendiwa/geometry/grid/constructors/RectangularHulls.kt
+++ src/main/kotlin/org/tendiwa/geometry/grid/constructors/RectangularHulls.kt
@@ -4,6 +4,7 @@ import org.tendiwa.geometry.grid.gridSegments.GridSegment
 import org.tendiwa.geometry.grid.gridSegments.end
 import org.tendiwa.geometry.grid.gridSegments.start
 import org.tendiwa.geometry.grid.rectangles.GridRectangle
+import org.tendiwa.geometry.grid.tiles.Tile
 import org.tendiwa.math.integers.segmentLength
 
 /**
@@ -19,3 +20,28 @@ val GridSegment.rectangularHull: GridRectangle
         this.start.y segmentLength this.end.y
     )
 
+val Iterable<Tile>.rectangularHull: GridRectangle
+    get() {
+        var minX = this.first().x
+        var minY = this.first().y
+        var maxX = this.first().x
+        var maxY = this.first().y
+        for (tile in this) {
+            if (tile.x < minX) {
+                minX = tile.x
+            } else if (tile.x > maxX) {
+                maxX = tile.x
+            }
+            if (tile.y < minY) {
+                minY = tile.y
+            } else if (tile.y > maxY) {
+                maxY = tile.y
+            }
+        }
+        return GridRectangle(
+            minX,
+            minY,
+            minX segmentLength maxX,
+            minY segmentLength maxY
+        )
+    }
