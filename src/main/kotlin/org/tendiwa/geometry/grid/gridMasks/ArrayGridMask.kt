c8420ab71736ea12f9da793d5633e69c7c20d599
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-10 09:37:51 +0500

    Implement ArrayGridMask

---
+++ src/main/kotlin/org/tendiwa/geometry/grid/gridMasks/ArrayGridMask.kt
@@ -0,0 +1,35 @@
+package org.tendiwa.geometry.grid.gridMasks
+
+class ArrayGridMask(
+    mask: BoundedGridMask
+) : BoundedGridMask {
+    override val hull = mask.hull
+
+    val bits: Array<BooleanArray> =
+        Array(this.hull.width, { BooleanArray(this.hull.height) })
+
+    init {
+        hull.forEachTile {
+            x, y ->
+            if (mask.contains(x, y)) {
+                add(x, y)
+            }
+        }
+    }
+
+    private fun add(absX: Int, absY: Int) {
+        bits[relativeX(absX)][relativeY(absY)] = true
+    }
+
+    override fun contains(x: Int, y: Int): Boolean {
+        return if (hull.contains(x, y)) {
+            bits[relativeX(x)][relativeY(y)]
+        } else {
+            false
+        }
+    }
+
+    private fun relativeX(absoluteX: Int) = absoluteX - hull.x
+
+    private fun relativeY(absoluteY: Int) = absoluteY - hull.y
+}
---
+++ src/test/kotlin/org/tendiwa/geometry/grid/gridMasks/ArrayGridMaskTest.kt
@@ -0,0 +1,18 @@
+package org.tendiwa.geometry.grid.gridMasks
+
+import org.junit.Test
+import org.tendiwa.geometry.grid.directions.OrdinalDirection.NW
+import org.tendiwa.geometry.grid.rectangles.GridRectangle
+import org.tendiwa.geometry.grid.rectangles.corner
+import org.tendiwa.geometry.grid.tiles.move
+import kotlin.test.assertFalse
+
+class ArrayGridMaskTest {
+    @Test fun doesNotContainTilesOutsideHull() {
+        val hull = GridRectangle(1, 2, 3, 4)
+        assertFalse(
+            ArrayGridMask(hull)
+                .contains(hull.corner(NW).move(-1, 0))
+        )
+    }
+}
