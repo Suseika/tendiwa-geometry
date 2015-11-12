90d02992dd8e5757e308926c833b4fb34f7ec25c
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-12 14:13:03 +0500

    Make GridSegment an interface with a constructor function of the same name

--- src/main/kotlin/org/tendiwa/geometry/grid/segments/GridSegment.kt
+++ src/main/kotlin/org/tendiwa/geometry/grid/segments/GridSegment.kt
@@ -1,65 +1,14 @@
 package org.tendiwa.geometry.grid.segments
 
-import org.tendiwa.geometry.grid.constructors.rectangleTo
 import org.tendiwa.geometry.grid.masks.BoundedGridMask
-import org.tendiwa.geometry.grid.rectangles.GridRectangle
 import org.tendiwa.geometry.grid.tiles.Tile
-import java.util.*
 
 /**
  * A segment formed by tiles using
  * [Bresenham line algorithm](https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm)
  */
-class GridSegment(val start: Tile, val end: Tile) : BoundedGridMask {
-    override val hull: GridRectangle
-        get() = start rectangleTo end
-
-    override val tiles: Set<Tile> by lazy {
-        tilesList.toSet()
-    }
-
+interface GridSegment : BoundedGridMask {
+    val start: Tile
+    val end: Tile
     val tilesList: List<Tile>
-        get() = bresenhamLine(start, end)
-
-    override fun contains(x: Int, y: Int): Boolean =
-        tiles.contains(Tile(x, y))
-
-    /**
-     * Implementation of
-     * [Bresenham line algorithm](https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm)
-     */
-    private fun bresenhamLine(start: Tile, end: Tile): List<Tile> {
-        val w = end.x - start.x
-        val h = end.y - start.y
-        val dx1 = Integer.signum(w)
-        var dx2 = Integer.signum(w)
-        val dy1 = Integer.signum(h);
-        var dy2 = 0;
-        var longest = Math.abs(w);
-        var shortest = Math.abs(h);
-        if (!(longest > shortest)) {
-            val tmp = longest
-            longest = shortest
-            shortest = tmp
-            dy2 = Integer.signum(h);
-            dx2 = 0;
-        }
-        val line = ArrayList<Tile>(longest)
-        var numerator = longest shr 1
-        var x = start.x
-        var y = start.y
-        for (i in 0..longest) {
-            line.add(Tile(x, y))
-            numerator += shortest
-            if (numerator >= longest) {
-                numerator -= longest
-                x += dx1
-                y += dy1
-            } else {
-                x += dx2
-                y += dy2
-            }
-        }
-        return line
-    }
 }
