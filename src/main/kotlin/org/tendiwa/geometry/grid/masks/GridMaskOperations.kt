16ad4b2e09557fbf999229e657b637da50f4c85e
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-12 06:53:33 +0500

    Implement GridMask.boundedBy extension method

--- src/main/kotlin/org/tendiwa/geometry/grid/masks/GridMaskOperations.kt
+++ src/main/kotlin/org/tendiwa/geometry/grid/masks/GridMaskOperations.kt
@@ -1,7 +1,15 @@
 package org.tendiwa.geometry.grid.masks
 
+import org.tendiwa.geometry.grid.rectangles.GridRectangle
 import org.tendiwa.geometry.grid.tiles.Tile
 
 fun GridMask.contains(tile: Tile): Boolean
     = contains(tile.x, tile.y)
 
+fun GridMask.boundedBy(rectangle: GridRectangle): BoundedGridMask =
+    object : BoundedGridMask {
+        override val hull = rectangle
+
+        override fun contains(x: Int, y: Int): Boolean =
+            this@boundedBy.contains(x, y)
+    }
