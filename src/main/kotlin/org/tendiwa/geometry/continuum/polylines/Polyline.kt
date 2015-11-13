5a85ff850e80c4b0e487e73970e7fd100614e86f
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-13 14:22:57 +0500

    Make Polyline implement Shape

--- src/main/kotlin/org/tendiwa/geometry/continuum/polylines/Polyline.kt
+++ src/main/kotlin/org/tendiwa/geometry/continuum/polylines/Polyline.kt
@@ -1,7 +1,11 @@
 package org.tendiwa.geometry.continuum.polylines
 
 import org.tendiwa.geometry.continuum.points.Point
+import org.tendiwa.geometry.continuum.shapes.Shape
 
 data class Polyline(
     val points: List<Point>
-)
+) : Shape {
+    override fun iterator(): Iterator<Point> =
+        points.iterator()
+}
