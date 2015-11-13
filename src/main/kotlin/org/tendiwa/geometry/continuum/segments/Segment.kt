fbe43bcbac1ec2e1b1596b85da0fb0413514b06a
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-13 14:24:29 +0500

    Make Segment implement Shape

--- src/main/kotlin/org/tendiwa/geometry/continuum/segments/Segment.kt
+++ src/main/kotlin/org/tendiwa/geometry/continuum/segments/Segment.kt
@@ -1,8 +1,16 @@
 package org.tendiwa.geometry.continuum.segments
 
 import org.tendiwa.geometry.continuum.points.Point
+import org.tendiwa.geometry.continuum.shapes.Shape
 
+/**
+ * A line segment.
+ */
 data class Segment(
     val start: Point,
     val end: Point
-)
+) : Shape {
+    override fun iterator(): Iterator<Point> {
+        return listOf(start, end).iterator()
+    }
+}
