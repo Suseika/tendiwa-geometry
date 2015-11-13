0621e13548f075b06302a0b5d8f32c45a1c05f77
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-13 14:22:08 +0500

    Make Rectangle implement Shape

--- src/main/kotlin/org/tendiwa/geometry/continuum/rectangles/Rectangle.kt
+++ src/main/kotlin/org/tendiwa/geometry/continuum/rectangles/Rectangle.kt
@@ -1,5 +1,8 @@
 package org.tendiwa.geometry.continuum.rectangles
 
+import org.tendiwa.geometry.continuum.points.Point
+import org.tendiwa.geometry.continuum.shapes.Shape
+
 /**
  * Axis-parallel rectangle or points in ℝ×ℝ.
  */
@@ -8,4 +11,13 @@ data class Rectangle(
     val y: Double,
     val width: Double,
     val height: Double
-)
+) : Shape {
+    override fun iterator(): Iterator<Point> =
+        listOf(
+            Point(x, y),
+            Point(maxX, y),
+            Point(maxX, maxY),
+            Point(x, maxY)
+        )
+            .iterator()
+}
