8d66cc698021463a21440af3970a60b6870e4a30
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-13 14:23:28 +0500

    Make Polygon implement Shape

--- src/main/kotlin/org/tendiwa/geometry/continuum/polygons/Polygon.kt
+++ src/main/kotlin/org/tendiwa/geometry/continuum/polygons/Polygon.kt
@@ -1,10 +1,14 @@
 package org.tendiwa.geometry.continuum.polygons
 
 import org.tendiwa.geometry.continuum.points.Point
+import org.tendiwa.geometry.continuum.shapes.Shape
 
 /**
  * A polygon.
  */
 data class Polygon(
     val points: List<Point>
-)
+) : Shape {
+    override fun iterator(): Iterator<Point> =
+        points.iterator()
+}
