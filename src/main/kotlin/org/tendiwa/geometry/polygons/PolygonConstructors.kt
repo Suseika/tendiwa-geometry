c2a7e17cd023d1b6784f0ebbd6cfc96e86c504b4
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-15 19:32:25 +0500

    Make Polygon an interface with a sneaky constructor instead of data class

--- src/main/kotlin/org/tendiwa/geometry/polygons/PolygonConstructors.kt
+++ src/main/kotlin/org/tendiwa/geometry/polygons/PolygonConstructors.kt
@@ -3,5 +3,9 @@ package org.tendiwa.geometry.polygons
 import org.tendiwa.geometry.points.Point
 
 fun Polygon(vararg points: Point): Polygon =
-    Polygon(points.asList())
+    DefaultPolygon(points.asList())
 
+fun Polygon(points: List<Point>): Polygon =
+    DefaultPolygon(points)
+
+private data class DefaultPolygon(override val points: List<Point>) : Polygon
