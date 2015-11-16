c06896ac7cb0aa418bb4c54b8fb1ebeea76359f7
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-16 14:48:50 +0500

    Add a check that the list of Points to construct a Polygon has at least 3 points

--- src/main/kotlin/org/tendiwa/geometry/polygons/PolygonConstructors.kt
+++ src/main/kotlin/org/tendiwa/geometry/polygons/PolygonConstructors.kt
@@ -2,10 +2,20 @@ package org.tendiwa.geometry.polygons
 
 import org.tendiwa.geometry.points.Point
 
-fun Polygon(vararg points: Point): Polygon =
-    DefaultPolygon(points.asList())
+fun Polygon(a: Point, b: Point, c: Point, vararg rest: Point): Polygon =
+    DefaultPolygon(listOf(a, b, c, *rest))
 
 fun Polygon(points: List<Point>): Polygon =
-    DefaultPolygon(points)
+    DefaultPolygon(points.apply { validatePointList(this) })
+
+private fun validatePointList(points: List<Point>) {
+    if (points.size < 3) {
+        throw IllegalArgumentException(
+            "There must be at least 3 points to construct a polygon; you " +
+                "provided ${points.size}: $points"
+        )
+    }
+}
 
 private data class DefaultPolygon(override val points: List<Point>) : Polygon
+
--- src/test/kotlin/org/tendiwa/geometry/polygons/PolygonTest.kt
+++ src/test/kotlin/org/tendiwa/geometry/polygons/PolygonTest.kt
@@ -3,8 +3,7 @@ package org.tendiwa.geometry.polygons
 import org.junit.Test
 import org.tendiwa.geometry.constructors.segmentTo
 import org.tendiwa.geometry.points.Point
-import org.tendiwa.geometry.polygons.Polygon
-import org.tendiwa.geometry.polygons.segments
+import kotlin.test.assertFails
 import kotlin.test.assertFalse
 
 class PolygonTest {
@@ -20,5 +19,11 @@ class PolygonTest {
         assertFalse(polygon.segments.contains(c segmentTo b))
         assertFalse(polygon.segments.contains(a segmentTo c))
         assert(polygon.segments.size == 3)
+    }
+
+    @Test fun failsWhenTooFewPointsAreProvided() {
+        assertFails { Polygon(listOf()) }
+        assertFails { Polygon(listOf(Point(0.0, 0.0))) }
+        assertFails { Polygon(listOf(Point(0.0, 0.0), Point(0.0, 2.3))) }
     }
 }
