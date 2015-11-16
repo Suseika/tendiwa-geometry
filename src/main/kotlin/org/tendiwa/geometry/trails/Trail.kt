763f04859791b60f812bf3966a65eefe95529b04
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-16 14:28:30 +0500

    Implement Trail

--- src/main/kotlin/org/tendiwa/geometry/trails/Trail.kt
+++ src/main/kotlin/org/tendiwa/geometry/trails/Trail.kt
@@ -5,26 +5,39 @@ import org.tendiwa.plane.directions.Direction
 import java.util.*
 
 class Trail(val start: Point) {
-    private val points: MutableList<Point> =
+    private val mutablePoints: MutableList<Point> =
         ArrayList<Point>().apply { add(start) }
+
+    val points: List<Point>
+        get() = mutablePoints
 
     private var last: Point = start
 
     fun move(dx: Double, dy: Double) {
         last = Point(last.x + dx, last.y + dy)
-        points.add(last)
+        mutablePoints.add(last)
     }
 
     fun moveX(dx: Double) {
-        last = Point(last.x+dx, last.y)
-        points.add(last)
+        last = Point(last.x + dx, last.y)
+        mutablePoints.add(last)
     }
 
     fun moveY(dy: Double) {
-        last = Point(last.x, last.y+dy)
+        last = Point(last.x, last.y + dy)
+        mutablePoints.add(last)
+    }
+
+    fun moveTo(point: Point) {
+        last = point
+        mutablePoints.add(last)
     }
 
     fun move(distance: Double, direction: Direction) {
-        last = Point(last.x+direction.dx, last.y+ direction.dy)
+        last = Point(
+            last.x + direction.dx * distance,
+            last.y + direction.dy * distance
+        )
+        mutablePoints.add(last)
     }
 }
