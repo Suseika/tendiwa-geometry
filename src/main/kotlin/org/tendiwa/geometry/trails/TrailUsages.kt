fc69da9023f9f226faabdeba0b0c6279a170eb11
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-17 21:05:12 +0500

    Create sneaky constructors for Polygon and Polyline that fill up a Trail

--- src/main/kotlin/org/tendiwa/geometry/trails/TrailUsages.kt
+++ src/main/kotlin/org/tendiwa/geometry/trails/TrailUsages.kt
@@ -1,10 +1,20 @@
 package org.tendiwa.geometry.trails
 
-import org.tendiwa.geometry.polylines.Polyline
+import org.tendiwa.geometry.points.Point
 import org.tendiwa.geometry.polygons.Polygon
+import org.tendiwa.geometry.polylines.Polyline
 
 val Trail.polygon: Polygon
     get() = Polygon(points)
 
 val Trail.polyline: Polyline
     get() = Polyline(points)
+
+fun Polyline(x: Double, y: Double, movements: Trail.() -> Unit): Polyline =
+    Polyline(Point(x, y), movements)
+
+fun Polyline(point: Point, movements: Trail.() -> Unit): Polyline =
+    Trail(point).apply(movements).polyline
+
+fun Polygon(x: Double, y: Double, movements: Trail.()-> Unit): Polygon =
+    Trail(x, y).apply(movements).polygon
