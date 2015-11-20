bca6aa167d1f4c6e0eba1dbf5cf409c6115f0ca4
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-20 16:44:05 +0500

    Implement comparing points linewise

--- src/main/kotlin/org/tendiwa/geometry/points/PointOperations.kt
+++ src/main/kotlin/org/tendiwa/geometry/points/PointOperations.kt
@@ -5,3 +5,10 @@ import org.tendiwa.math.doubles.isCloseToZero
 infix fun Point.reallyCloseTo(another: Point): Boolean =
     (this.x - another.x).isCloseToZero && (this.y - another.y).isCloseToZero
 
+fun comparePointsLinewise(a: Point, b: Point): Int {
+    val signum = Math.signum(a.x - b.x).toInt();
+    if (signum == 0) {
+        return Math.signum(a.y - b.y).toInt();
+    }
+    return signum;
+}
