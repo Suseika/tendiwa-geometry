76148ba2d26e3ccee40a86ebc84bcb40d972ddc9
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-20 22:55:57 +0500

    Implement transforming a vector to a point it points to

--- src/main/kotlin/org/tendiwa/geometry/vectors/VectorProperties.kt
+++ src/main/kotlin/org/tendiwa/geometry/vectors/VectorProperties.kt
@@ -1,5 +1,6 @@
 package org.tendiwa.geometry.vectors
 
+import org.tendiwa.geometry.points.Point
 import org.tendiwa.math.doubles.sqrt
 import org.tendiwa.math.doubles.square
 
@@ -8,3 +9,7 @@ val Vector.magnitude: Double
 
 val Vector.isZero: Boolean
     get()  = x == 0.0 && y == 0.0
+
+val Vector.point: Point
+    get() = Point(x, y)
+
