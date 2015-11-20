231d151b2dcd6f44c652abbc42aa57f36f319e1f
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-20 16:52:24 +0500

    Add constructor Rectangle(Point, Dimension)

--- src/main/kotlin/org/tendiwa/geometry/rectangles/RectangleConstructors.kt
+++ src/main/kotlin/org/tendiwa/geometry/rectangles/RectangleConstructors.kt
@@ -1,5 +1,11 @@
 package org.tendiwa.geometry.rectangles
 
+import org.tendiwa.geometry.Dimension
+import org.tendiwa.geometry.points.Point
+
+fun Rectangle(start: Point, size: Dimension): Rectangle =
+    Rectangle(start.x, start.y, size.width, size.height)
+
 fun rectangleFromRanges(
     minX: Double,
     maxX: Double,
