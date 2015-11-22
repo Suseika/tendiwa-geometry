f8963d02cdd46828c4c9b348e06a176ca95d9e61
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-23 01:11:01 +0500

    Implement spanning Segments from a Point in a Direction

--- src/main/kotlin/org/tendiwa/geometry/constructors/Spans.kt
+++ src/main/kotlin/org/tendiwa/geometry/constructors/Spans.kt
@@ -3,12 +3,16 @@ package org.tendiwa.geometry.constructors
 import org.tendiwa.geometry.points.Point
 import org.tendiwa.geometry.points.move
 import org.tendiwa.geometry.segments.Segment
+import org.tendiwa.plane.directions.Direction
 
 /**
  * Creates a [Segment] by placing its another end *relative* to this point.
  */
 fun Point.spanSegment(dx: Double, dy: Double): Segment =
     Segment(this, this.move(dx, dy))
+
+fun Point.spanSegment(direction: Direction, distance: Double): Segment =
+    Segment(this, this.move(direction, distance))
 
 fun Point.spanHorizontalSegment(dx: Double): Segment =
     Segment(this, this.move(dx, 0.0))
