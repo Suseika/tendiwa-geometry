209e23e311ea5ff1bc4bc88241990133caa2dad6
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-24 14:55:26 +0500

    Make Segment fail when its start and end are the same point

--- src/main/kotlin/org/tendiwa/geometry/segments/Segment.kt
+++ src/main/kotlin/org/tendiwa/geometry/segments/Segment.kt
@@ -10,6 +10,13 @@ data class Segment(
     val start: Point,
     val end: Point
 ) : SegmentGroup {
+    init {
+        if (start == end) {
+            throw IllegalArgumentException(
+                "Start can't be equal to end (they're both $start)"
+            )
+        }
+    }
     override val points: List<Point>
         get() = listOf(start, end)
 
