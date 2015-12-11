0a59abb6e8077b26889eb7ca7ee40d82543117aa
Georgy Vlasov <wlasowegor@gmail.com>
2015-12-11 06:34:58 +0500

    Add a test for Ray

--- src/test/kotlin/org/tendiwa/geometry/rays/RayTest.kt
+++ src/test/kotlin/org/tendiwa/geometry/rays/RayTest.kt
@@ -1,13 +1,17 @@
 package org.tendiwa.geometry.rays
 
 import org.junit.Test
+import org.tendiwa.geometry.points.AnyPoint
+import org.tendiwa.geometry.points.move
+import org.tendiwa.plane.directions.OrdinalDirection
+import kotlin.test.assertEquals
 
 class RayTest {
     @Test
     fun pointOnRay() {
-//        assertEquals(
-//            AnyPoint().moveKing(OrdinalDirection.SE, 3.8),
-//            Ray(AnyPoint(), Math.PI / 4).pointOnRay(3.8)
-//        )
+        assertEquals(
+            AnyPoint().move(OrdinalDirection.SE, 3.8),
+            Ray(AnyPoint(), Math.PI / 4).pointOnRay(3.8)
+        )
     }
 }
