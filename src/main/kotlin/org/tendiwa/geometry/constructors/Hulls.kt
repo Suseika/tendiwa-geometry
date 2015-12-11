d98d611eea8a30ad99c04fbbbec6a0ebed5c34a4
Georgy Vlasov <wlasowegor@gmail.com>
2015-12-11 16:37:23 +0500

    Remove unused extension method

--- src/main/kotlin/org/tendiwa/geometry/constructors/Hulls.kt
+++
@@ -1,9 +0,0 @@
-package org.tendiwa.geometry.constructors
-
-import org.tendiwa.geometry.circles.Circle
-import org.tendiwa.geometry.dimensions.Dimension
-import org.tendiwa.geometry.rectangles.Rectangle
-
-val Circle.rectangularHull: Rectangle
-    get() = centeredRectangle(center, Dimension(radius * 2, radius * 2))
-
