46967ebbc2c3f2af206db185e937e67385b2c3fb
Georgy Vlasov <wlasowegor@gmail.com>
2015-12-12 06:47:42 +0500

    Implement Rectangle.strictlyContains

--- src/main/kotlin/org/tendiwa/plane/geometry/rectangles/RectangleOperations.kt
+++ src/main/kotlin/org/tendiwa/plane/geometry/rectangles/RectangleOperations.kt
@@ -5,3 +5,5 @@ import org.tendiwa.plane.geometry.points.Point
 fun Rectangle.contains(point: Point): Boolean =
     point.x in x..maxX && point.y in y..maxY
 
+fun Rectangle.strictlyContains(point: Point): Boolean =
+    point.x > x && point.x < maxX && point.y > y && point.y < maxY
