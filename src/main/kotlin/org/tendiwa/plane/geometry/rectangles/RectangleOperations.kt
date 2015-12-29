2c74cd49963de4631c1a2ea23bbaa1a659e3fe8f
Georgy Vlasov <wlasowegor@gmail.com>
2015-12-30 02:13:26 +0500

    Implement Rectangle.side

--- src/main/kotlin/org/tendiwa/plane/geometry/rectangles/RectangleOperations.kt
+++ src/main/kotlin/org/tendiwa/plane/geometry/rectangles/RectangleOperations.kt
@@ -1,9 +1,19 @@
 package org.tendiwa.plane.geometry.rectangles
 
+import org.tendiwa.plane.directions.CardinalDirection
 import org.tendiwa.plane.geometry.points.Point
+import org.tendiwa.plane.geometry.segments.Segment
 
 fun Rectangle.contains(point: Point): Boolean =
     point.x in x..maxX && point.y in y..maxY
 
 fun Rectangle.strictlyContains(point: Point): Boolean =
     point.x > x && point.x < maxX && point.y > y && point.y < maxY
+
+fun Rectangle.side(direction: CardinalDirection): Segment =
+    when (direction) {
+        CardinalDirection.N -> segments[0]
+        CardinalDirection.E -> segments[1]
+        CardinalDirection.S -> segments[2]
+        CardinalDirection.W -> segments[3]
+    }
