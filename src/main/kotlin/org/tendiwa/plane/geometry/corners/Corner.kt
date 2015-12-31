58ac1fbe311bc44fdeb7e3ed2444f682f157d6b4
Georgy Vlasov <wlasowegor@gmail.com>
2015-12-31 22:56:27 +0500

    Implement Corner

---
+++ src/main/kotlin/org/tendiwa/plane/geometry/corners/Corner.kt
@@ -0,0 +1,7 @@
+package org.tendiwa.plane.geometry.corners
+
+import org.tendiwa.plane.geometry.points.Point
+import org.tendiwa.plane.geometry.vectors.VectorSector
+
+data class Corner(val point: Point, val sector: VectorSector)
+
