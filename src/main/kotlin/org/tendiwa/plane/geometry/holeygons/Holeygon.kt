c1ed6760c1787e434472feac144db722c75efd9a
Georgy Vlasov <wlasowegor@gmail.com>
2016-01-03 06:55:16 +0500

    Make Holeygon a SegmentGroup

--- src/main/kotlin/org/tendiwa/plane/geometry/holeygons/Holeygon.kt
+++ src/main/kotlin/org/tendiwa/plane/geometry/holeygons/Holeygon.kt
@@ -1,6 +1,9 @@
 package org.tendiwa.plane.geometry.holeygons
 
+import org.tendiwa.plane.geometry.points.Point
 import org.tendiwa.plane.geometry.polygons.Polygon
+import org.tendiwa.plane.geometry.segments.Segment
+import org.tendiwa.plane.geometry.shapes.SegmentGroup
 
 /**
  * Polygon with holes.
@@ -14,5 +17,11 @@ data class Holeygon
  * @param enclosing Polygon that encloses the [holes].
  * @param holes Holes inside the [enclosing] polygon.
  */
-(val enclosing: Polygon, val holes: Collection<Polygon>)
+(val enclosing: Polygon, val holes: Collection<Polygon>) : SegmentGroup {
+    override val points: Collection<Point>
+        get() = enclosing.points + holes.flatMap { it.points }
+
+    override val segments: Collection<Segment>
+        get() = enclosing.segments + holes.flatMap { it.segments }
+}
 
