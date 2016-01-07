eec64a9c7ffbc19787bc83d29380d8e731c84390
Georgy Vlasov <wlasowegor@gmail.com>
2016-01-08 04:36:57 +0500

    Implement constructing a Ray from a Segment

--- src/main/kotlin/org/tendiwa/plane/geometry/rays/Ray.kt
+++ src/main/kotlin/org/tendiwa/plane/geometry/rays/Ray.kt
@@ -1,10 +1,15 @@
 package org.tendiwa.plane.geometry.rays
 
 import org.tendiwa.plane.geometry.points.Point
+import org.tendiwa.plane.geometry.segments.Segment
+import org.tendiwa.plane.geometry.segments.vector
+import org.tendiwa.plane.geometry.vectors.direction
 
 /**
  * [Ray on Wikipedia](https://en.wikipedia.org/wiki/Line_(geometry)#Ray)
  * @param start Beginning of the ray.
  * @param direction Direction in radians.
  */
-data class Ray(val start: Point, val direction: Double)
+data class Ray(val start: Point, val direction: Double) {
+    constructor(guide: Segment) : this(guide.start, guide.vector.direction)
+}
