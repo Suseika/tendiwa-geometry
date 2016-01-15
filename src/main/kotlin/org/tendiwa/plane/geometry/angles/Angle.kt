5419f7125f1a84bc38ae69aba3cc27cb6c897307
Georgy Vlasov <wlasowegor@gmail.com>
2016-01-15 21:42:18 +0500

    Extract DirectionFan from Angle

--- src/main/kotlin/org/tendiwa/plane/geometry/angles/Angle.kt
+++ src/main/kotlin/org/tendiwa/plane/geometry/angles/Angle.kt
@@ -1,12 +1,7 @@
 package org.tendiwa.plane.geometry.angles
 
-import org.tendiwa.math.angles.AngularMeasure
 import org.tendiwa.plane.directions.Direction
-import org.tendiwa.plane.directions.RadianDirection
 import org.tendiwa.plane.geometry.points.Point
-import org.tendiwa.plane.geometry.rays.Ray
-import org.tendiwa.tools.butIf
-import java.lang.Math.PI
 
 /**
  * Figure formed by two rays, called the sides of the angle.
@@ -22,37 +17,3 @@ data class Angle
     val cw: Direction,
     val ccw: Direction
 )
-
-val Angle.measure: AngularMeasure
-    get() = cw.counterClockwiseAngle(ccw)
-
-val Angle.bisector: Ray
-    get() = Ray(
-        point,
-        ccw.radians
-            .butIf(
-                { it < cw.radians },
-                { it + PI * 2 }
-            )
-            .let { (it + cw.radians) / 2 }
-            .let { RadianDirection(it) }
-    )
-
-private fun Angle.isTranszero(): Boolean =
-    cw.radians > ccw.radians
-
-fun Angle.contains(direction: Direction): Boolean {
-    val isTranszero = isTranszero()
-    val orderedCcwRadians = ccw.radians
-        .butIf(
-            { isTranszero },
-            { it + PI * 2 }
-        )
-    val orderedTargetRadians = direction.radians
-        .butIf(
-            { it < cw.radians },
-            { it + PI * 2 }
-        )
-    return orderedTargetRadians >= cw.radians
-        && orderedTargetRadians <= orderedCcwRadians
-}
