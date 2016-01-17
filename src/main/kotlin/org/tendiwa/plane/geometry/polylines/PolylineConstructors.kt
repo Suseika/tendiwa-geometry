de3d56111c719d581b47916c526ce9ed9d42da4d
Georgy Vlasov <wlasowegor@gmail.com>
2016-01-17 20:35:43 +0500

    Add vararg constructor for Polyline

--- src/main/kotlin/org/tendiwa/plane/geometry/polylines/PolylineConstructors.kt
+++ src/main/kotlin/org/tendiwa/plane/geometry/polylines/PolylineConstructors.kt
@@ -5,4 +5,7 @@ import org.tendiwa.plane.geometry.points.Point
 fun Polyline(points: List<Point>): Polyline =
     DefaultPolyline(points)
 
+fun Polyline(vararg points: Point): Polyline =
+    Polyline(points.toList())
+
 private data class DefaultPolyline(override val points: List<Point>) : Polyline
