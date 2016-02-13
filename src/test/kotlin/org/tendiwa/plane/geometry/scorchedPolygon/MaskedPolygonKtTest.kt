1c8feb4e680adbd5ff98f750d788a77ded9c78df
Georgy Vlasov <wlasowegor@gmail.com>
2016-02-14 01:24:49 +0500

    Don't use PerimeterPiece in this test

--- src/test/kotlin/org/tendiwa/plane/geometry/scorchedPolygon/MaskedPolygonKtTest.kt
+++ src/test/kotlin/org/tendiwa/plane/geometry/scorchedPolygon/MaskedPolygonKtTest.kt
@@ -7,9 +7,7 @@ import org.tendiwa.math.sliders.CircularMask
 import org.tendiwa.math.sliders.CircularSlider
 import org.tendiwa.math.sliders.Slider1Sphere
 import org.tendiwa.math.sliders.Slider1Sphere.Size
-import org.tendiwa.plane.geometry.polygons.masked.Scorch
 import org.tendiwa.plane.geometry.polygons.masked.mask
-import org.tendiwa.plane.geometry.polygons.masked.scorch
 import org.tendiwa.plane.geometry.rectangles.Rectangle
 import kotlin.test.assertEquals
 
@@ -41,15 +39,17 @@ class MaskedPolygonKtTest {
     @Test
     fun `non-intersecting scorches increase numbers of burns and unburns by 1`() {
         Rectangle(0.0, 0.0, 10.0, 10.0)
-            .scorch(
-                listOf(
-                    Scorch(
-                        CircularSlider(0.1),
-                        9.0
-                    ),
-                    Scorch(
-                        CircularSlider(0.6),
-                        10.0
+            .mask(
+                CircularMask(
+                    listOf(
+                        Slider1Sphere(
+                            CircularSlider(0.1),
+                            Size(0.2)
+                        ),
+                        Slider1Sphere(
+                            CircularSlider(0.6),
+                            Size(0.2)
+                        )
                     )
                 )
             )
