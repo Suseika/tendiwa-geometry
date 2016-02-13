348ab59e0714f394ebd3c28eef9cad45ebadd81a
Georgy Vlasov <wlasowegor@gmail.com>
2016-02-13 23:16:03 +0500

    Implement disjoining a CutPolygon

---
+++ src/main/kotlin/org/tendiwa/plane/geometry/polygons/cut/Disjoining.kt
@@ -0,0 +1,44 @@
+package org.tendiwa.plane.geometry.polygons.cut
+
+import org.tendiwa.collections.circularSubList
+import org.tendiwa.collections.loopedLinks
+import org.tendiwa.plane.geometry.polylines.Polyline
+import org.tendiwa.tools.argumentsConstraint
+import java.util.*
+
+/**
+ * Disjoins this cut polygon at cut points into [Polyline]s.
+ * @return Polylines of points of this polygon, with the start of first
+ * polyline being at the first cut point, and the rest of polylines going
+ * polygonwise.
+ * @throws IllegalArgumentException If there are 0 or an odd number of cuts.
+ */
+fun CutPolygon.disjoin(): List<Polyline> {
+    argumentsConstraint(
+        !cuts.isEmpty() && cuts.size % 2 == 0,
+        {
+            "CutPolygon should contain an even number of cuts that is > 0;" +
+                "CutPolygon has ${cuts.size} cuts"
+        }
+    )
+    val polygon = this.toPolygon()
+    return cutsIndices()
+        .loopedLinks
+        .map { polygon.points.circularSubList(it.first, it.second+1) }
+        .map { Polyline(it) }
+}
+
+private fun CutPolygon.cutsIndices(): List<Int> {
+    val indices = ArrayList<Int>(cuts.size)
+    var cutIndex = 0
+    val points = this.toPolygon().points
+    points.forEachIndexed {
+        i, point ->
+        if (cutIndex < cuts.size && point == cuts[cutIndex]) {
+            indices.add(i)
+            cutIndex++
+        }
+    }
+    return indices
+}
+
---
+++ src/test/kotlin/org/tendiwa/plane/geometry/polygons/cut/DisjoiningKtTest.kt
@@ -0,0 +1,50 @@
+package org.tendiwa.plane.geometry.polygons.cut
+
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.ExpectedException
+import org.tendiwa.plane.geometry.polygons.perimeter
+import org.tendiwa.plane.geometry.polylines.length
+import org.tendiwa.plane.geometry.rectangles.Rectangle
+import org.tendiwa.tools.assertAlmostEquals
+import org.tendiwa.tools.expectIllegalArgument
+import kotlin.test.assertEquals
+
+class DisjoiningKtTest {
+    @JvmField @Rule val expectRule = ExpectedException.none()
+
+    @Test
+    fun disjoin() {
+        val polygon = Rectangle(0.0, 0.0, 10.0, 10.0)
+        polygon
+            .cut(0.2, 0.8)
+            .disjoin()
+            .apply {
+                assertEquals(2, size)
+                assertAlmostEquals(
+                    polygon.perimeter,
+                    this.map { it.length }.sum()
+                )
+            }
+    }
+
+    @Test
+    fun `fails to disjoin if the number of cuts is 0`() {
+        expectRule.expectIllegalArgument(
+            "CutPolygon should contain an even number of cuts that is > 0"
+        )
+        Rectangle(0.0, 0.0, 10.0, 10.0)
+            .cut()
+            .disjoin()
+    }
+
+    @Test
+    fun `fails to disjoin if the number of cuts is odd`() {
+        expectRule.expectIllegalArgument(
+            "CutPolygon should contain an even number of cuts that is > 0"
+        )
+        Rectangle(0.0, 0.0, 10.0, 10.0)
+            .cut(0.1, 0.2, 0.3)
+            .disjoin()
+    }
+}
