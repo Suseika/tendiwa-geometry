a1b13234dbcfc242dd0b7a3d5497f90d3bf41efd
Georgy Vlasov <wlasowegor@gmail.com>
2016-02-14 13:50:57 +0500

    Make SegmentPath an interface

--- src/main/kotlin/org/tendiwa/plane/geometry/paths/SegmentPath.kt
+++ src/main/kotlin/org/tendiwa/plane/geometry/paths/SegmentPath.kt
@@ -1,5 +1,6 @@
 package org.tendiwa.plane.geometry.paths
 
+import org.tendiwa.collections.withoutLast
 import org.tendiwa.plane.geometry.points.Point
 import org.tendiwa.plane.geometry.segments.Segment
 import org.tendiwa.plane.geometry.shapes.SegmentGroup
@@ -10,14 +11,14 @@ import org.tendiwa.tools.argumentConstraint
  * A line of segment that may contain the same point twice. Thus, it may
  * be equivalent to a [Polyline] or a [Polygon].
  */
-class SegmentPath(override val points: List<Point>) : SegmentGroup {
-    init {
-        argumentConstraint(
-            points,
-            { it.size > 1 },
-            { "point list must contain at least 2 points" }
-        )
-    }
+interface SegmentPath : SegmentGroup {
+    override val points: List<Point>
+
+    /**
+     * Almost same as [points], but it first and last elements are identical
+     * in case the [SegmentPath] is closed.
+     */
+    val steps: List<Point>
 
     override val segments: List<Segment>
         get() = points
@@ -25,11 +26,34 @@ class SegmentPath(override val points: List<Point>) : SegmentGroup {
             .mapIndexed { i, point -> Segment(point, points[i + 1]) }
 
     fun isClosed(): Boolean =
-        points[0] == points.last()
+        steps[0] == steps.last()
 
     fun isOpen(): Boolean =
         !isClosed()
 }
 
+private class GenericSegmentPath(
+    override val steps: List<Point>
+) : SegmentPath {
+    override val points: List<Point>
+        get() =
+        if (steps.first() == steps.last()) {
+            steps.withoutLast()
+        } else {
+            steps
+        }
+
+    init {
+        argumentConstraint(
+            points,
+            { it.size > 1 },
+            { "point list must contain at least 2 points" }
+        )
+    }
+}
+
+fun SegmentPath(steps: List<Point>): SegmentPath =
+    GenericSegmentPath(steps)
+
 fun SegmentPath(start: Point, operations: Trail.() -> Unit): SegmentPath =
-    SegmentPath(Trail(start).apply(operations).points)
+    GenericSegmentPath(Trail(start).apply(operations).points)
